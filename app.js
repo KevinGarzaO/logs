17707d55472a532bc731602186d41228a90cb75a
Kevin Garza <kevin_4569@msn.com>
2022-02-03 17:54:33 -0600

    prueba de logs

---
+++ app.js
@@ -0,0 +1,51 @@
+//Función para escribir logs del aplicativo
+function log(texto){
+  const fs = require('fs');
+  const date = formatted_date();
+  const dateHour = formatted_date_hours();
+  
+  fs.appendFileSync(`./logs/log_${date}.txt`, `${dateHour} -> ${texto}\n`, (err) => {
+    if (err) throw err;
+    console.log('The text was appended to file!');
+  });
+}
+
+
+//Función para sacar fecha formateada
+function formatted_date()
+{
+   var result="";
+   var d = new Date();
+   const day = zeroFill(d.getDate(),2);
+   const month = zeroFill((d.getMonth()+1), 2)
+   const year = d.getFullYear()
+   result += `${day}${month}${year}`
+   return result;
+}
+
+//Función para sacar fecha con hora formateada 
+function formatted_date_hours()
+{
+   var result="";
+   var d = new Date();
+   const day = zeroFill(d.getDate(),2);
+   const month = zeroFill((d.getMonth()+1), 2);
+   const year = d.getFullYear();
+   const hour = zeroFill(d.getHours(),2)+":"+zeroFill(d.getMinutes(), 2)+":"+ zeroFill(d.getSeconds(), 2);
+
+   result += `${day}/${month}/${year} ${hour}`
+   return result;
+}
+
+//Función para poner ceros a la izquierda
+function zeroFill( number, width )
+{
+  width -= number.toString().length;
+  if ( width > 0 )
+  {
+    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+  }
+  return number + ""; // siempre devuelve tipo cadena
+}
+
+log("Escribir otra cosa 2")
